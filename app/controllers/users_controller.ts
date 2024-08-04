d926a991add36602477ec84103e9d3c673726aac
queonias <queones@gmail.com>
2024-08-04 10:23:28 -0300

    feat(controller): adicionar método signup para registro de usuários

---
+++ app/controllers/users_controller.ts
@@ -0,0 +1,23 @@
+import type { HttpContext } from '@adonisjs/core/http';
+import User from '../models/user.js';
+
+export default class UsersController {
+    async signup({ request, response }: HttpContext) {
+        const { email, password } = request.only(['email', 'password'])
+
+
+        // Verifica se o usuário já existe
+        const existingUser = await User.findBy('email', email)
+        if (existingUser) {
+            return response.badRequest({ message: 'User already exists' })
+        }
+
+        // Cria o novo usuário
+        const user = new User()
+        user.email = email
+        user.password = password
+        await user.save()
+
+        return response.created({ message: 'User created successfully' })
+    }
+}
