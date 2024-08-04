c5b4367c3c7a7b8033c775f5c6e734c8f0061214
queonias <queones@gmail.com>
2024-08-04 19:06:36 -0300

    feat(middleware): adicionar middleware de autenticação nas rotas

--- start/routes.ts
+++ start/routes.ts
@@ -1,22 +1,30 @@
-import router from '@adonisjs/core/services/router'
+import router from '@adonisjs/core/services/router';
+import { middleware } from './kernel.js';
 
 const UsersController = () => import('#controllers/users_controller');
 const ClientsController = () => import('#controllers/clients_controller');
 const ProductsController = () => import('#controllers/products_controller');
 const SalesController = () => import('#controllers/sales_controller');
 
+// Rotas de autenticação
 router.post('/api/users/signup', [UsersController, 'signup']);
 router.post('/api/users/login', [UsersController, 'login']);
 
-router.get('/api/clients/list', [ClientsController, 'index']);
-router.get('/api/clients/:id', [ClientsController, 'show']);
-router.post('/api/clients/save', [ClientsController, 'store']);
-router.put('/api/clients/:id', [ClientsController, 'update']);
-router.delete('/api/clients/:id', [ClientsController, 'destroy']);
+router.group(() => {
+    // Rotas de clientes
+    router.get('/api/clients/list', [ClientsController, 'index']);
+    router.get('/api/clients/:id', [ClientsController, 'show']);
+    router.post('/api/clients/save', [ClientsController, 'store']);
+    router.put('/api/clients/:id', [ClientsController, 'update']);
+    router.delete('/api/clients/:id', [ClientsController, 'destroy']);
 
-router.get('/api/products/list', [ProductsController, 'index']);
-router.get('/api/products/:id', [ProductsController, 'show']);
-router.post('/api/products/save', [ProductsController, 'store']);
-router.put('/api/products/:id', [ProductsController, 'update']);
+    // Rotas de produtos
+    router.get('/api/products/list', [ProductsController, 'index']);
+    router.get('/api/products/:id', [ProductsController, 'show']);
+    router.post('/api/products/save', [ProductsController, 'store']);
+    router.put('/api/products/:id', [ProductsController, 'update']);
 
-router.post('/api/sales/save', [SalesController, 'store']);
+    // Rotas de vendas
+    router.post('/api/sales/save', [SalesController, 'store']);
+}).use(middleware.auth()); // Middleware de autenticação
+
