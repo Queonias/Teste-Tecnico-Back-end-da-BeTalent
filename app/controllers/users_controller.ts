7f94981005e543ce04690de3ed6307227eb71664
queonias <queones@gmail.com>
2024-08-04 11:39:49 -0300

    fix(controller): corrigir método login para usar verificação de credenciais e gerar token JWT

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,10 +1,9 @@
 import type { HttpContext } from '@adonisjs/core/http';
 import User from '../models/user.js';
-import hash from '@adonisjs/core/services/hash'
 
 export default class UsersController {
     async signup({ request, response }: HttpContext) {
-        const { email, password } = request.only(['email', 'password'])
+        const { email, password } = request.all()
 
 
         // Verifica se o usuário já existe
@@ -24,17 +23,8 @@ export default class UsersController {
 
     async login({ request, response, auth }: HttpContext) {
         const { email, password } = request.all()
-        // Verifica se o usuário existe
-        const user = await User.verifyCredentials(email, password)
-        if (!user) {
-            return response.badRequest({ message: 'Invalid email or password' })
-        }
-
-        // Verifica a senha
-        const isSame = await hash.verify(user.password, password)
-        if (!isSame) {
-            return response.badRequest({ message: 'Invalid email or password' })
-        }
+        // Verifica se as credenciais estão corretas
+        const user: User = await User.verifyCredentials(email, password)
 
         // Gera o token JWT
         const token = await auth.use('jwt').generate(user)
