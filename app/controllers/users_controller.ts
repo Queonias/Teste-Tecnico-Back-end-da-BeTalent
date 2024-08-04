67782d3acb2fbeb764b36c6aef36fa494aa61df4
queonias <queones@gmail.com>
2024-08-04 11:15:56 -0300

    feat(controller): adicionar método login para autenticação de usuários com JWT

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,5 +1,6 @@
 import type { HttpContext } from '@adonisjs/core/http';
 import User from '../models/user.js';
+import hash from '@adonisjs/core/services/hash'
 
 export default class UsersController {
     async signup({ request, response }: HttpContext) {
@@ -19,5 +20,25 @@ export default class UsersController {
         await user.save()
 
         return response.created({ message: 'User created successfully' })
+    }
+
+    async login({ request, response, auth }: HttpContext) {
+        const { email, password } = request.all()
+        // Verifica se o usuário existe
+        const user = await User.verifyCredentials(email, password)
+        if (!user) {
+            return response.badRequest({ message: 'Invalid email or password' })
+        }
+
+        // Verifica a senha
+        const isSame = await hash.verify(user.password, password)
+        if (!isSame) {
+            return response.badRequest({ message: 'Invalid email or password' })
+        }
+
+        // Gera o token JWT
+        const token = await auth.use('jwt').generate(user)
+
+        return response.ok({ token })
     }
 }
