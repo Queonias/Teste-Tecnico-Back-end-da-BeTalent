d47364f843020c431c35dfa0bc550b3db63120bb
queonias <queones@gmail.com>
2024-08-04 16:18:09 -0300

    feat(routes): Adicionada rota PUT /api/clients/:id para permitir a atualização de informações de um cliente

--- start/routes.ts
+++ start/routes.ts
@@ -9,3 +9,4 @@ router.post('/api/users/login', [UsersController, 'login']);
 router.get('/api/clients/list', [ClientsController, 'index']);
 router.get('/api/clients/:id', [ClientsController, 'show']);
 router.post('/api/clients/save', [ClientsController, 'store']);
+router.put('/api/clients/:id', [ClientsController, 'update']);
