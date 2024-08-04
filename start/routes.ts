c87a1a13ab72321bfeb6e839a41ddfb4161a5595
queonias <queones@gmail.com>
2024-08-04 18:32:58 -0300

    feat(routes): adicionar rota para listar produtos

--- start/routes.ts
+++ start/routes.ts
@@ -2,6 +2,7 @@ import router from '@adonisjs/core/services/router'
 
 const UsersController = () => import('#controllers/users_controller');
 const ClientsController = () => import('#controllers/clients_controller');
+const ProductsController = () => import('#controllers/products_controller');
 
 router.post('/api/users/signup', [UsersController, 'signup']);
 router.post('/api/users/login', [UsersController, 'login']);
@@ -11,3 +12,5 @@ router.get('/api/clients/:id', [ClientsController, 'show']);
 router.post('/api/clients/save', [ClientsController, 'store']);
 router.put('/api/clients/:id', [ClientsController, 'update']);
 router.delete('/api/clients/:id', [ClientsController, 'destroy']);
+
+router.get('/api/products/list', [ProductsController, 'index']);
